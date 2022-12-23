f8ef7837213245ca8458c1e9ade1122503416ee5
gofogo <promoted.go@gmail.com>
2022-12-23 18:57:50 +0900

    modify: layout 네이브 레이아웃 잡는 중

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="w-full flex flex-row max-w-5xl">
+      <div className="flex flex-row bg-red-500 justify-center items-center ">
+        <div>ㅁ</div>
+        <div>Youtube</div>
+      </div>
+      <div className="flex flex-row w-full bg-red-50 justify-center items-center h-10 ">
+        <input type="text" className="bg-green-500 w-2/3" />
+        <div>ㅁ</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 const Root = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center" >
       <Navbar />
       <Outlet />
     </div>
