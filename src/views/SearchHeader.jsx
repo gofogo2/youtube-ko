d4dd42ffddeeba2e5db2277a9f632424ae05f742
gofogo <promoted.go@gmail.com>
2022-12-27 18:19:17 +0900

    init: searchheader

--- src/views/SearchHeader.jsx
+++ src/views/SearchHeader.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-import icon_youtube from "../assets/images/icon_youtube.png";
-import icon_search from "../assets/images/icon_search.svg";
 
 const SearchHeader = () => {
   return (
-    <div className="flex flex-row items-center justify-start w-full h-12 max-w-5xl px-5 mt-5 border-b-[1px] pb-5 t-5 border-black">
-      <div className="flex flex-row items-center justify-center ">
-        <img src={icon_youtube} width={30} alt="" />
-        <div  >Youtube</div>
-      </div>
-      <div className="flex flex-row items-center justify-center w-full h-10 x-5 ">
-        <input type="text" placeholder="Search"  className="  pl-5 w-2/3 h-full border-red-900 border-y-[1px] border-l-[1px] rounded-l-xl" />
-        <img src={icon_search} width={70} alt="" className="h-full border-[1px] border-red-900 rounded-r-xl px-5" />
-      </div>
+   <header>
+    <div>
+
     </div>
+    <form></form>
+   </header>
   );
 };
 
