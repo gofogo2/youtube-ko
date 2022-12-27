c5776e05fecd5bb7519cb2eb09fe5a7018fe33d9
gofogo <promoted.go@gmail.com>
2022-12-27 23:01:50 +0900

    add: 헤더 기능

--- src/views/SearchHeader.jsx
+++ src/views/SearchHeader.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BsYoutube } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const SearchHeader = () => {
-  return (
-   <header>
-    <div>
 
-    </div>
-    <form></form>
-   </header>
+  const navigate = useNavigate();
+
+  const [text, setText] = useState("");
+  const {keyword} = useParams();
+
+  useEffect(()=>{setText(keyword||'')},[keyword]);
+
+  const handleSumit = (e) => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  };
+  return (
+    <header>
+      <div>
+        <Link to="/" >
+        <BsYoutube />
+        <h1>Youtube</h1>
+        </Link>
+      </div>
+      <form onSubmit={handleSumit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <button>
+          <BsSearch />
+        </button>
+      </form>
+    </header>
   );
 };
 
--- src/views/Videos.jsx
+++ src/views/Videos.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Videos = () => {
+
+  const {keyword} = useParams();
+
   return (
     <div>
-      Videos
+      Videos {keyword?`${keyword}`:'hot'}
     </div>
   )
 }
