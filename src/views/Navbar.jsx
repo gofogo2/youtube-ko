4dde2a5e9216f60b056ea0dce54775c1801b96f7
gofogo <promoted.go@gmail.com>
2022-12-27 15:18:41 +0900

    add:네비바 및 아이콘 추가

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import icon_youtube from "../assets/images/icon_youtube.png";
+import icon_search from "../assets/images/icon_search.svg";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex flex-row max-w-5xl">
-      <div className="flex flex-row bg-red-500 justify-center items-center ">
-        <div>ㅁ</div>
-        <div>Youtube</div>
+    <div className="flex flex-row items-center justify-start w-full h-12 max-w-5xl px-5 mt-5 border-b-[1px] pb-5 t-5 border-black">
+      <div className="flex flex-row items-center justify-center ">
+        <img src={icon_youtube} width={30} alt="" />
+        <div  >Youtube</div>
       </div>
-      <div className="flex flex-row w-full bg-red-50 justify-center items-center h-10 ">
-        <input type="text" className="bg-green-500 w-2/3" />
-        <div>ㅁ</div>
+      <div className="flex flex-row items-center justify-center w-full h-10 x-5 ">
+        <input type="text" placeholder="Search"  className="  pl-5 w-2/3 h-full border-red-900 border-y-[1px] border-l-[1px] rounded-l-xl" />
+        <img src={icon_search} width={70} alt="" className="h-full border-[1px] border-red-900 rounded-r-xl px-5" />
       </div>
     </div>
   );
