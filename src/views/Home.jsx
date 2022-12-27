0a858d878b0b0b41f49fd99bb7be221f1ba6f52f
gofogo <promoted.go@gmail.com>
2022-12-27 16:08:15 +0900

    Add All

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [list, setList] = useState([]);
   useEffect(() => {
     fetch("/datas/data.json")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setList(data);
+        // console.log(data);
       });
   });
 
-  return <div></div>;
+  return (
+    <div className="grid grid-flow-col grid-rows-5" >
+      {list.map((item) => {
+        return (
+          <div>
+            <div>{item.title}</div>
+            <img src={item.src} alt="" />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
